8de8b32523e40a81a0871007d8629ddf665704a6
Tim Case <tim@2drops.net>
2017-01-17 12:24:49 +0700

    Add bind action creators

--- src/containers/app.js
+++ src/containers/app.js
@@ -1,30 +1,12 @@
 import React, { Component } from 'react';
 import Main from '../components/main';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import { addTodo, updateTodo, deleteTodo, markCompleted,
-  markAllCompleted } from '../actions';
+import * as TodoActions from '../actions';
 
-const mapDispatchToProps = (dispatch) => {
-  return {
-    callbacks: {
-      create: (text) => {
-        dispatch(addTodo(text));
-      },
-      update: (id, text) => {
-        dispatch(updateTodo(id, text))
-      },
-      deleteTodo: (id) => {
-        dispatch(deleteTodo(id))
-      },
-      markCompleted: (id) => {
-        dispatch(markCompleted(id))
-      },
-      markAllCompleted: () => {
-        dispatch(markAllCompleted())
-      }
-    }
-  }
-};
+const mapDispatchToProps = dispatch => ({
+    actions: bindActionCreators(TodoActions, dispatch)
+})
 
 const mapStateToProps = (state) => {
   return { todos: state.todos };
@@ -34,7 +16,7 @@ class App extends Component {
 
   render() {
     return (
-      <Main todos={this.props.todos} callbacks={this.props.callbacks} />
+      <Main todos={this.props.todos} callbacks={this.props.actions} />
     );
   }
 }

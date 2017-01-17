d0f368689a9d25bd08bebdfdb90bb4fee97cd44e
Tim Case <tim@2drops.net>
2017-01-17 11:45:43 +0700

    Add hmr with redbox

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './containers/app';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
@@ -10,9 +9,51 @@ import { Provider } from 'react-redux';
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(
-  <Provider store={store} >
-    <App store={store}/>
-  </Provider>,
-  document.getElementById('root')
-);
+// Save a reference to the root element for reuse
+const rootEl = document.getElementById("root");
+
+// Create a reusable render method that we can call more than once
+let render = () => {
+    // Dynamically import our main App component, and render it
+  const App = require("./containers/app").default;
+
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        rootEl
+    );
+};
+
+if(module.hot) {
+    // Support hot reloading of components
+    // and display an overlay for runtime errors
+    const renderApp = render;
+    const renderError = (error) => {
+        const RedBox = require("redbox-react").default;
+        ReactDOM.render(
+            <RedBox error={error} />,
+            rootEl,
+        );
+    };
+
+    // In development, we wrap the rendering function to catch errors,
+    // and if something breaks, log the error and render it to the screen
+    render = () => {
+        try {
+            renderApp();
+        }
+        catch(error) {
+            console.error(error);
+            renderError(error);
+        }
+    };
+
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+  module.hot.accept("./containers/app", () => {
+        setTimeout(render);
+    });
+}
+
+render();

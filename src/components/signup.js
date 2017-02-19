a3cc8e3959f73a94d435f13a068725ef6702cfcc
Tim Case <tim@2drops.net>
2017-02-19 12:28:49 +0700

    Add redux-form reset form

--- src/components/signup.js
+++ src/components/signup.js
@@ -1,27 +1,38 @@
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
-import update from 'immutability-helper';
 import './login.css';
-import SignupForm from './signup_form';
 
 class Signup extends Component {
   constructor(){
     super()
     this.state = {
-      fields: {email: '',
-               password: '',
-               name: ''}
+      email: '',
+      password: '',
+      name: ''
     }
   }
-
   componentWillMount() {
     if (this.props.isAuthenticated) {
       browserHistory.push("/");
     }
   }
 
-  fieldChange = (field, value) => {
-    this.setState(update(this.state, {fields: {[field]: {$set: value}}}))
+  handleNameChange = (e) => {
+    this.setState({
+      name: e.target.value
+    });
+  }
+
+  handleEmailChange = (e) => {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  handlePasswordChange = (e) => {
+    this.setState({
+      password: e.target.value
+    });
   }
 
   get redirectTo(){
@@ -41,12 +52,12 @@ class Signup extends Component {
       }
   }
 
-  handleSubmit = () => {
-    console.log(this.state.fields.name);
+  handleSubmit = (e) => {
+    e.preventDefault();
     this.props.actions.signupUser(
-      this.state.fields.name,
-      this.state.fields.email,
-      this.state.fields.password,
+      this.state.name,
+      this.state.email,
+      this.state.password,
       this.redirectTo)
   }
 
@@ -57,14 +68,17 @@ class Signup extends Component {
         <div className="col-sm-6 col-md-4 col-md-offset-4">
             <h1 className="text-center login-title">Sign up to Booter</h1>
             <div className="account-wall">
+
                 <img className="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
                   alt=""/>
-                <SignupForm
-                  fields={this.state.fields}
-                  onChange={this.fieldChange}
-                  onValid={this.handleSubmit}
-                  onInvalid={() => alert('Error!')}
-                />
+                <form className="form-signin">
+                {this.errorAlert}
+                  <input type="text" onChange={this.handleNameChange} className="form-control" placeholder="Name" required autoFocus />
+                  <input type="text" onChange={this.handleEmailChange} className="form-control" placeholder="Email" required autoFocus />
+                  <input type="password" onChange={this.handlePasswordChange} className="form-control" placeholder="Password" required />
+                <button onClick={this.handleSubmit} className="btn btn-lg btn-primary btn-block" type="submit">
+                    Sign up</button>
+                </form>
             </div>
         </div>
     </div>

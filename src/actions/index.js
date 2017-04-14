a10ebd3e912e0259f4aed948d465cb921cc3c717
Tim Case <tim@2drops.net>
2017-04-14 10:44:01 +0700

    Remove commented out function

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,22 +5,3 @@ export const updateTodo = createAction("update a todo");
 export const deleteTodo = createAction("delete a todo");
 export const markAsCompletedTodo = createAction("mark a todo as completed");
 export const markAllAsCompletedTodo = createAction("mark all todos as completed");
-
-
-// import * as types from '../constants/action_types'
-
-// function makeActionCreator(type, ...argNames) {
-//   return function(...args) {
-//     let action = { type }
-//     argNames.forEach((arg, index) => {
-//       action[argNames[index]] = args[index]
-//     })
-//     return action
-//   }
-// }
-
-// export const addTodo = makeActionCreator(types.ADD_TODO, 'text');
-// export const updateTodo = makeActionCreator(types.UPDATE_TODO, 'id', 'text');
-// export const deleteTodo = makeActionCreator(types.DELETE_TODO, 'id');
-// export const markCompleted = makeActionCreator(types.COMPLETE_TODO, 'id');
-// export const markAllCompleted = makeActionCreator(types.COMPLETE_ALL);
